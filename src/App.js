4579bd8661a0f9bd79d53083e5fb209b339bf133
ankush <ankushk385@gmail.com>
2021-04-30 19:24:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
+import {useState} from 'react';
 import jsonData from './data/photos.json';
 import DataTable from './component/DataTable';
 
 const get50 = (data)=>{
   return data.slice(0,50);
 }
+
 function App() {
+
+  const [photoes, setPhotoes] = useState(get50(jsonData))
+
+  const loadMore = ()=>{
+    console.log("load more")
+  }
   return (
     <div className="App">
       <DataTable
-      items={get50(jsonData)}
+      loadMore={loadMore}
+      items={photoes}
       RenderHead = {()=>(
         <tr>
           <th>ID</th>
--- src/component/DataTable.jsx
+++ src/component/DataTable.jsx
@@ -1,10 +1,22 @@
 import React,{useEffect,useRef} from 'react'
 
-const DataTable = ({items,RenderHead,RenderRow}) => {
+const DataTable = ({items,RenderHead,RenderRow,loadMore}) => {
 
-    
+    const ref = useRef(null);
+    useEffect(() => {
+        document.addEventListener("scroll",renderScroll)
+        return ()=> document.removeEventListener("scroll",renderScroll);
+    }, [])
+
+const renderScroll = ()=>{
+    const cy = window.scrollY;
+    const tbh = ref.current.offsetHeight;
+    const thres = 1000;
+    if(tbh-cy-thres < 0 ) loadMore();
+   
+}
     return (
-        <table>
+        <table ref={ref} >
             <thead> {RenderHead()}</thead>
             <tbody> {items.map((row)=>RenderRow(row))}  </tbody>
         </table>
